08c368c4f105ec73aaf4f01a77384725d3b702e2
Alexander Diewald <Diewi@diewald-net.com>
2018-03-07 22:13:51 +0100

    Adjust dtc to provide libs needed for multirom by Android.mk.
    * Requires "util.c", built as a small lib for linking.
    * Requires header exports from libfdt.
    * We need these entries in Android.mk, since soong was
      introduced from Oreo onwards.
    
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,49 @@
 # Copyright 2016 The Android Open Source Project
 
 LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libfdt
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/libfdt
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/libfdt
+
+LOCAL_SRC_FILES := \
+    libfdt/fdt.c \
+    libfdt/fdt_ro.c \
+    libfdt/fdt_wip.c \
+    libfdt/fdt_sw.c \
+    libfdt/fdt_rw.c \
+    libfdt/fdt_strerror.c \
+    libfdt/fdt_empty_tree.c \
+    libfdt/fdt_addresses.c \
+    libfdt/fdt_overlay.c
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libdtcutil
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+
+LOCAL_SRC_FILES := \
+    util.c
+
+LOCAL_STATIC_LIBRARIES := \
+    libfdt
+
+include $(BUILD_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_CFLAGS := \
